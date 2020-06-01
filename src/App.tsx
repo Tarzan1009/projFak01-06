0995b32c5e072cc1ee5abf59c5105247b8c47942
Piotr Praszkiewicz <piotrek1009@gmail.com>
2020-06-01 19:02:30 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
 
+    const [data, setData] = useState({posts: []});
+
+    useEffect(() => {
+        fetch('http://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then((data) => {
+                setData(data);
+            })
+    }, []);
+
+    return (
+        <Router>
+            <ul>
+                {data.posts.map(item => (
+                    <li key={item.id}>
+                        <Link to={"/posts/" + item.id}>{item.title}</Link>
+                    </li>
+                ))}
+            </ul>
+            <Route path="/posts/:id" component={Post}/>
+        </Router>
+    );
+};
 export default App;
+
+const Post = (props) => {
+
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch('http://jsonplaceholder.typicode.com/posts/' + props.match.params.id)
+            .then(res => res.json())
+            .then((data) => {
+                setData(data);
+            })
+    }, []);
+
+    return (
+        <Router>
+            <h1>{data.title}</h1>
+            <h2>{data.body}</h2>
+            <Link to={"/users/" + data.userID}>{data.userID}</Link>
+            <Route path="/users/:id"> <User/> </Route>
+        </Router>
+    );
+};
+
+const User = (props) => {
+
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch('http://jsonplaceholder.typicode.com/users/' + props.match.params.id)
+            .then(res => res.json())
+            .then((data) => {
+                setData(data);
+            })
+    }, []);
+
+    return (
+        <div>
+            <h1>{data.name}</h1>
+        </div>
+    );
+};
